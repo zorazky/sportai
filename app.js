fa52168782e98cbe5847c2264eddfc4f836439cd
zorazky <oswaldo.vazquez@gmail.com>
2020-06-10 13:40:25 -0500

    materialize version

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var dropdowns = document.querySelectorAll('.dropdown-trigger')
+    for (var i = 0; i < dropdowns.length; i++){
+        M.Dropdown.init(dropdowns[i]);
+    }
+
+});
